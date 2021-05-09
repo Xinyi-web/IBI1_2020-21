b24376e1ef1778727407e0b37fc402b3cc2cf5b2
Xinyi-web <79914761+Xinyi-web@users.noreply.github.com>
2021-05-09 14:10:17 +0800

    Update r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,8 +1,10 @@
-#df-r,r=1.2,n=84,after 5 times str()
+#df-r,r=1.2,n=84,after 5 times
 count=5
 r=1.2
 n=84
+#Use loop statements to implement functions
 while count>0:
   count=count-1
-  n=n*r
+  #Notice the difference between the number of people infected and the number of times you have to multiply
+  n=n*(r+1)
 print("the number of individuals is "+str(n))
