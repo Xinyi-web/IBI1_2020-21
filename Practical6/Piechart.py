07d6e791e27ddf8ee37844e2910e991f340364ba
Xinyi-web <79914761+Xinyi-web@users.noreply.github.com>
2021-05-09 14:44:20 +0800

    Update Piechart.py

--- Practical6/Piechart.py
+++ Practical6/Piechart.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 labels='USA','India','Brazil','Russia','UK'
+sizes = (29862124,11285561,11205972,4360823,4234924)
+# Do not use explode to get a relatively objective figure
 plt.pie(sizes,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)
 plt.axis('equal')
 plt.show()
